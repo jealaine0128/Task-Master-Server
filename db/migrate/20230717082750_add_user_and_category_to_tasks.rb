af3342bca2ff1b8e66a6b87eeef0fada07f8810b
jealaine0128 <125180585+jealaine0128@users.noreply.github.com>
2023-07-22 19:33:31 +0800

    Delete 20230717082750_add_user_and_category_to_tasks.rb

--- db/migrate/20230717082750_add_user_and_category_to_tasks.rb
+++
@@ -1,6 +0,0 @@
-class AddUserAndCategoryToTasks < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :tasks, :user, null: false, foreign_key: true
-    add_reference :categories, :user, null: true, foreign_key: true
-  end
-end
